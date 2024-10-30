3482c518d6f2dc32a00a2a40acf3e3d61811d6b4
Emerson.H <emerson.ads1010@gmail.com>
2024-10-30 14:00:31 -0300

    Adicionei Motor Animações, Movimentação animada, Parte 2 completa ;)

--- src/classes/Player.js
+++ src/classes/Player.js
@@ -1,12 +1,28 @@
+import { INITIAL_FRAMES, PATH_ENGINE_IMAGE, PATH_ENGINESPRITES, PATH_SPACESHIP_IMAGE } from "../utils/constants.js";
+import Projectile from "./Projectile.js";
+
 class Player {
     constructor(canvasWidth, canvasHeight){
-        this.width = 100;
-        this.height = 100;
+        this.width = 48 * 2;
+        this.height = 48 * 2;
         this.velocity = 6
         this.position = {
             x: canvasWidth / 2 - this.width / 2,
             y: canvasHeight - this.height - 30,
-        }
+        };
+
+        this.image = this.getImage(PATH_SPACESHIP_IMAGE);
+        this.engineImage = this.getImage(PATH_ENGINE_IMAGE);
+        this.engineStrites = this.getImage(PATH_ENGINESPRITES);
+
+        this.sx = 0;
+        this.framesCounter = INITIAL_FRAMES;
+    }
+
+    getImage(path) {
+        const image = new Image();
+        image.src = path;
+        return image;
     }
 
     moveLeft(){
@@ -16,15 +32,61 @@ class Player {
     moveRight(){
         this.position.x += this.velocity;
     }
-     draw(ctx){
-        ctx.fillStyle = "red";
-        ctx.fillRect(
+
+    draw(ctx){
+        ctx.drawImage(
+            this.image,
             this.position.x,
             this.position.y,
             this.width,
             this.height
         );
-     }
+
+        ctx.drawImage(
+            this.engineStrites,
+            this.sx,
+            0,
+            48,
+            48,
+            this.position.x,
+            this.position.y + 10,
+            this.width,
+            this.height
+        );
+
+        ctx.drawImage(
+            this.engineImage,
+            this.position.x,
+            this.position.y + 8,
+            this.width,
+            this.height
+        );
+
+        this.update();
+    }
+
+    update() {
+
+        if(this.framesCounter === 0){
+           this.sx = this.sx === 96 ? 0: this.sx + 48;
+            this.framesCounter = INITIAL_FRAMES;
+        }
+
+        this.framesCounter--;
+        
+    }
+
+    shoot(projectiles){
+        const p = new Projectile(
+            {
+                x: this.position.x + this.width / 2 - 1,
+                y: this.position.y + 2,
+            },
+            -10
+        );
+
+        projectiles.push(p);
+    }
 }
 
 export default Player;
